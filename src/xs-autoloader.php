fbe17716eab5909b8900f29ac9fe257be75f0740
Emran <emran@xpeedstudio.com>
2021-02-11 14:40:38 +0600

    v1.0.0

---
+++ src/xs-autoloader.php
@@ -0,0 +1,61 @@
+<?php 
+namespace XpeedStudio;
+
+class Xs_Autoloader{
+
+    private $namespace;
+
+    private $classdir;
+    
+	/**
+	 * Run autoloader.
+	 * Register a function as `__autoload()` implementation.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 */
+	public function __construct( $namespace = null, $classdir = '' ){
+        if($namespace == null){
+            return;
+        }
+        $this->namespace = $namespace;
+
+        $this->classdir = trailingslashit($classdir);
+
+		spl_autoload_register( [ $this, 'autoload' ] );
+    }
+    
+    /**
+	 * Autoload.
+	 * For a given class, check if it exist and load it.
+	 *
+	 * @since 1.0.0
+	 * @access private
+	 * @param string $class Class name.
+	 */
+	private function autoload( $class_name ) {
+
+        // If the class being requested does not start with our prefix
+        // we know it's not one in our project.
+        if ( 0 !== strpos( $class_name, $this->namespace ) ) {
+            return;
+        }
+        
+        $file_name = strtolower(
+            preg_replace(
+                [ '/\b'.$this->namespace.'\\\/', '/([a-z])([A-Z])/', '/_/', '/\\\/' ],
+                [ '', '$1-$2', '-', DIRECTORY_SEPARATOR],
+                $class_name
+            )
+        );
+
+        // Compile our path from the corosponding location.
+        $file = $this->classdir . $file_name . '.php';
+        
+        // If a file is found.
+        if ( file_exists( $file ) ) {
+            // Then load it up!
+            require_once( $file );
+        }
+    }
+}
